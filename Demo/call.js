9bf0391848ac46234ccdeac95bb380d7383df65a
Yorick Sijsling <yoricksijsling@gmail.com>
2010-10-19 15:28:53 +0200

    Call.js was just for remembering the code. Not used anywhere

--- Demo/call.js
+++
@@ -1,7 +0,0 @@
-                parent.getElements('.interactiveTable').each(function(el) {
-                        new SortList({
-                                elements: el.getElements('.sortlistitem'),
-                                sortLinks: el.getElements('.sorttagselect')
-                        })
-                })
-
